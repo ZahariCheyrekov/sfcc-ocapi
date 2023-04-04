21709b5e8a74c31cc9a64b8d0398ecc81ee18b3d
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-04 18:00:21 +0300

    Implement ocapi service

---
+++ src/services/ocapi-service.js
@@ -0,0 +1,40 @@
+import * as requester from '../api/requester';
+import { AUTH, getContentAssetUrl } from '../constants/endpoints';
+
+/**
+ * @function
+ * @description Returns access token for the current user
+ * @returns {String} Access Token
+ */
+export const getAccessToken = async () => {
+    const request = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-dw-client-id': `${process.env.REACT_APP_CLIENT_ID}`,
+        },
+        body: JSON.stringify({ type: 'guest' })
+    };
+
+    try {
+        const response = await fetch(AUTH, request);
+        const token = response.headers.get('authorization');
+
+        localStorage.setItem('token', token);
+        return token;
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+/**
+ * @function
+ * @description Returns content asset from the BM by the given cid
+ * @param {String} cid - Id of the given content asset
+ * @returns {Promise<Object>} Content Asset's body
+ */
+export const getContentAsset = async (cid) => {
+    const asset = await requester.get(getContentAssetUrl(cid));
+    return asset.c_body;
+};
