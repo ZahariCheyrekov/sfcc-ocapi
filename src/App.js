ccc3dbb9b3f56a9f0b09d58f73d8bed23cf25bd0
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-04 18:00:51 +0300

    Import app routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AppRoutes from './routes/AppRoutes';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AppRoutes />
       <Footer />
     </div>
   );
