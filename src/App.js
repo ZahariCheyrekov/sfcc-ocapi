7e988446886327a83d45f0e98eadf71fe9de3558
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-04 12:09:15 +0300

    Import header and footer components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Footer />
     </div>
   );
 }
