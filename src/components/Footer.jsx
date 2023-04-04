a169948079963e1f9a36b5d4e4fc93ea0c1f2c76
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-04 17:59:04 +0300

    Fix footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const { contentAsset } = useContentAsset("ocapi-footer-links");
 
   return (
-    <footer className="bg-dark text-white w-100 position-fixed bottom-0 ">
+    <footer className="bg-dark text-white w-100 position-fixed bottom-0">
       <div className="d-flex align-items-center justify-content-between w-75 m-auto">
         <section
           className="m-auto"
