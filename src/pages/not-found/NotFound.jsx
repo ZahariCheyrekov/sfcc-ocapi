1b5487aa685789cf452d4ad6a49763c11dd3720c
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-04 13:04:28 +0300

    Add not found page

---
+++ src/pages/not-found/NotFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <main className="main-not-found min-h-100 m-auto w-75 text-center pt-5">
+      <section className="section-not-found">
+        <h1 className="title-not-found">404</h1>
+        <h2 className="paragraph-not-found">
+          The requested resource is not available
+        </h2>
+      </section>
+    </main>
+  );
+};
+
+export default NotFound;
