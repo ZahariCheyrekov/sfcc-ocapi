df970717d4ce1fc28843d970059174238d7238f3
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-05 16:17:07 +0300

    Add cart provider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import AppRoutes from './routes/AppRoutes';
 
+import { CartProvider } from './contexts/CartContext';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AppRoutes />
-      <Footer />
+      <CartProvider>
+        <Header />
+        <AppRoutes />
+        <Footer />
+      </CartProvider>
     </div>
   );
 }
