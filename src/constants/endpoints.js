35498c52ac73ea9a9bc9498a8878b403d7be9ebc
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-06 15:54:05 +0300

    Configure endpoint methods

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -2,6 +2,37 @@ export const BASE_URL = `${process.env.REACT_APP_HOST}/s/Sites-${process.env.REA
 
 export const AUTH = `${BASE_URL}/customers/auth`;
 
-export const getContentAssetUrl = (cid) => `${BASE_URL}/content/${cid}`;
+export const CREATE_CART_URL = `${BASE_URL}/baskets`;
 
-export const getProductUrl = (pid) => `${BASE_URL}/products/${pid}?expand=availability,prices,images,variations,bundled_products`;
+/**
+ * @function
+ * @param {String} cid Id of the given content asset
+ * @returns {String} Content Asset url
+*/
+export const getContentAssetUrl = (cid) =>
+    `${BASE_URL}/content/${cid}`;
+
+/**
+ * @function
+ * @param {String} pid Id of the given product
+ * @returns {String} Product details url 
+*/
+export const getProductUrl = (pid) =>
+    `${BASE_URL}/products/${pid}?expand=availability,prices,images,variations,bundled_products`;
+
+/**
+ * @function
+ * @param {String} basketId Id of the created basket
+ * @returns {String} Url of the current basket with items in it
+*/
+export const getAddProductUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/items`;
+
+/**
+ * @function
+ * @param {String} basketId Id of the given basket
+ * @param {String} productId Id of the product to be removed
+ * @returns {String} Basket url for product removal
+*/
+export const getRemoveProductUrl = (basketId, productId) =>
+    `${BASE_URL}/baskets/${basketId}/items/${productId}`;
