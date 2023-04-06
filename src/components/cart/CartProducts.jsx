bbaf05b356e28f1c2b087f8dbdb72acfd49e8a87
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-06 16:25:33 +0300

    Render cart products

---
+++ src/components/cart/CartProducts.jsx
@@ -0,0 +1,22 @@
+import ProductTile from "./ProductTile";
+
+const CartProducts = ({ cart }) => {
+  console.log(cart);
+  return (
+    <>
+      <article className="cart-product-item cart-product">
+        {cart.product_items?.map((item, key) => (
+          <ProductTile item={item} key={key} currency={cart.currency} />
+        ))}
+      </article>
+      <div className="row justify-content-end border-top border-bottom mb-3">
+        <h4 className="col-4 py-2 border-light">Product Total:</h4>
+        <h4 className="border-light py-2 col-4 ">
+          {cart.product_total.toFixed(2)} {cart.currency}
+        </h4>
+      </div>
+    </>
+  );
+};
+
+export default CartProducts;
