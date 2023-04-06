dab7ac5c52100d2290acfc6e4a768646bb30ccf3
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-06 16:00:24 +0300

    Splie product logic in multiple components

--- src/pages/product/ProductDetails.jsx
+++ src/pages/product/ProductDetails.jsx
@@ -3,7 +3,9 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { getProduct } from "../../services/product-service";
 
-import Carousel from "react-bootstrap/Carousel";
+import Product from "../../components/product/Product";
+import ProductBundle from "../../components/product/ProductBundle";
+
 const ProductDetails = () => {
   const navigate = useNavigate();
   const { pid } = useParams();
@@ -20,41 +22,15 @@ const ProductDetails = () => {
       }
     };
     fetchProduct();
-  }, [pid]);
+  }, [pid, navigate]);
 
   return (
-    product && (
-      <main>
-        <div className="container pt-4">
-          <div className="row">
-            <div className="col-12 col-md-4">
-              <Carousel>
-                {product.image_groups[0].images.map((image, key) => (
-                  <Carousel.Item className={`carousel-item `} key={key}>
-                    <img
-                      src={image.link}
-                      alt={image.title}
-                      loading="lazy"
-                      className="d-block w-100"
-                    />
-                  </Carousel.Item>
-                ))}
-              </Carousel>
-            </div>
-            <div className="col-12 col-md-8">
-              <div className="row">
-                <h1>{product.name}</h1>
-                <h3>{pid}</h3>
-                <h4>
-                  {product.price} {product.currency}
-                </h4>
-                <p>{product.page_description}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    )
+    product &&
+    (product?.type.bundle ? (
+      <ProductBundle productBundles={product} />
+    ) : (
+      <Product product={product} isBundle={false} />
+    ))
   );
 };
 
