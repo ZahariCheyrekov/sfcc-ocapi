be77c9820673f5829a5d760c004f981fc9152ff0
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-10 11:32:47 +0300

    Configure edpoints for differenet requests

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -4,6 +4,8 @@ export const AUTH = `${BASE_URL}/customers/auth`;
 
 export const CREATE_CART_URL = `${BASE_URL}/baskets`;
 
+export const CREATE_ORDER_URL = `${BASE_URL}/orders`;
+
 /**
  * @function
  * @param {String} cid Id of the given content asset
@@ -36,3 +38,53 @@ export const getAddProductUrl = (basketId) =>
 */
 export const getRemoveProductUrl = (basketId, productId) =>
     `${BASE_URL}/baskets/${basketId}/items/${productId}`;
+
+/**
+* @function
+* @param {String} basketId Id of the given basket
+* @returns {String} Basket url for adding a shipping address
+*/
+export const getAddShippingAddressUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/shipments/me/shipping_address`;
+
+/**
+* @function
+* @param {String} basketId Id of the given basket
+* @returns {String} Basket url for adding a shipping method
+*/
+export const getAddShippingMethodUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/shipments/me/shipping_method`;
+
+/**
+* @function
+* @param {String} basketId Id of the given basket
+* @returns {String} Returns url for getting all of the basket shipping methods
+*/
+export const getShippingMethodsUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/shipments/me/shipping_methods`;
+
+/**
+* @function
+* @param {String} basketId Id of the given basket
+* @returns {String} Returns url for adding billing address to the order
+*/
+export const getAddBillingAddressUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/billing_address`;
+
+/**
+* @function
+* @param {String} basketId Id of the given basket
+* @returns {String} Returns url for getting all of the basket payment methods
+*/
+export const getPaymentMethodsUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/payment_methods`;
+
+/**
+* @function
+* @param {String} basketId Id of the given basket
+* @returns {String} Returns url for adding paymnet
+*/
+export const getAddPaymentUrl = (basketId) =>
+    `${BASE_URL}/baskets/${basketId}/payment_instruments`;
+
+
