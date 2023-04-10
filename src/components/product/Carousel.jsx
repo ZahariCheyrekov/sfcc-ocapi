99587a00d522db1637a556c6b34f6c0657f7e80d
Zahari <zahari.cheyreckov999@gmail.com>
2023-04-10 15:31:42 +0300

    Fix carousel component

--- src/components/product/Carousel.jsx
+++ src/components/product/Carousel.jsx
@@ -4,7 +4,7 @@ const ProductCarousel = ({ images }) => {
   return (
     <Carousel>
       {images.map((image, key) => (
-        <Carousel.Item className={`carousel-item `} key={key}>
+        <Carousel.Item className={`carousel-item`} key={key}>
           <img
             src={image.link}
             alt={image.title}
